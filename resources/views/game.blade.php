2593e0b8971543a449cc580bdfd8611e138f8a40
VssLocker <asslocker@vk.com>
2022-08-09 19:42:20 +0300

    card woerks

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -12,7 +12,7 @@
         <x-text.gamedate :game='$game'/>
     </div>
 
-    <div class="relative group w-full h-400px overflow-hidden rounded-3xl">
+    <div class="relative group w-full h-400px overflow-hidden rounded-3xl ring-2 ring-amber-500/75">
         <div id="map" class="h-[400px] w-full bg-[#303030]/25 scale-110">
             
         </div>
