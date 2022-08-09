2437594b274f0225baab60b38311a1a658b1e634
threeh <threeh1@vk.com>
2022-08-09 12:10:34 +0300

    text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,13 @@
         <x-text.gamedate/>
     </div>
     <div class="h-[400px] w-full bg-[#303030]/25 rounded-3xl">
-
+        <a href="{{ route('game', 1001) }}">1</a>
     </div>
 
     <div class="flex w-full justify-center py-6">
         <x-text.gamedate/>
     </div>
-    <div class="h-[400px] w-full bg-[#303030]/25 rounded-3xl"></div>
+    <div class="h-[400px] w-full bg-[#303030]/25 rounded-3xl">
+        <a href="{{ route('game', 1001) }}">1</a>
+    </div>
 @endsection
