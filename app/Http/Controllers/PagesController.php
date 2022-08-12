725087ed3af4f295708b59db23a5eefec966ef0c
VssLocker <asslocker@vk.com>
2022-08-12 15:36:51 +0300

    fix controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -15,39 +15,22 @@ class PagesController extends Controller
 {
     public function index() {
         $games = DB::table('games')->get();
-<<<<<<< HEAD
         return view('index', compact('games'));
-=======
-        $success = Session::get('success');
-        return view('index', compact('games', 'success'));
->>>>>>> 415e818625244c1a11036ed28ff4564edcf5bd5f
     }
 
     public function archive() {
-        $games = DB::table('games')->where('finished', '=', 1)->get();
-<<<<<<< HEAD
-        return view('archive', compact('games'));
-=======
-        $success = Session::get('success');
         $teams = DB::table('teams')->get();
-        return view('archive', compact('games', 'success'));
->>>>>>> 415e818625244c1a11036ed28ff4564edcf5bd5f
+        $games = DB::table('games')->where('finished', '=', 1)->get();
+        return view('archive', compact('games'));
     }
 
     public function game($id) {
         $first_cord = Game::find($id)->first_cord;
         $second_cord = Game::find($id)->second_cord;
         $game = Game::find($id);
-<<<<<<< HEAD
         $teams = Db::table('teams')->get('name');
-        return view('game', compact('first_cord', 'second_cord', 'game', 'teams'));
-=======
-
-        $success = Session::get('success');
-        $teams = DB::table('teams')->get();
         $teams_count = DB::table('teams')->count();
-        return view('game', compact('first_cord', 'second_cord', 'game', 'success', 'teams', 'teams_count'));
->>>>>>> 415e818625244c1a11036ed28ff4564edcf5bd5f
+        return view('game', compact('first_cord', 'second_cord', 'game', 'teams', 'teams_count'));
     }
 
     public function store_players(Request $request, $id) {
@@ -88,13 +71,5 @@ class PagesController extends Controller
                 ['success' => 'You have successfully subscribed to the newsletter!']
             );
         }
-<<<<<<< HEAD
-=======
-
-        DB::table('emails')->insert(['email' => $request->email, 'created_at' => now(), 'updated_at' => now()]);
-        return redirect()->back()->with(
-            ['success' => 'You have successfully subscribed to the newsletter!']
-        );
->>>>>>> 415e818625244c1a11036ed28ff4564edcf5bd5f
     }
 }
