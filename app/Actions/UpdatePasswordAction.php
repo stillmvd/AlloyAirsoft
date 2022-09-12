1eaf9eb2c14116e58c1075e1a6effaf43e952848
threeh <threeh1@vk.com>
2022-09-12 13:21:07 +0300

    change password

--- app/Actions/UpdatePasswordAction.php
+++ app/Actions/UpdatePasswordAction.php
@@ -5,20 +5,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UpdateLoginAction
+class UpdatePasswordAction
 {
     /**
-     * Обновляет логин админа, проверив его пароль
+     * Обновляет пароль админа
      *
      * @param Illuminate\Http\Request $request
      * @return void
      */
     public function update(Request $request)
     {
-        if(Hash::check($request->password, DB::table('users')->pluck('password')[0]))
+        if(Hash::check($request->passwordOld, DB::table('users')->pluck('password')[0]))
         {
             DB::table('users')->update([
-               'login' => $request->login,
+               'password' => Hash::make($request->passwordNew),
             ]);
             return true;
         }
