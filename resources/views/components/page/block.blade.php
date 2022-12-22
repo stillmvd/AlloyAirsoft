c1d6ab0828b5715c65c047124b0258522f3510a9
vsslocker <asslocker@vk.com>
2022-12-22 20:33:00 +0300

    creating price system

--- resources/views/components/page/block.blade.php
+++ resources/views/components/page/block.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->class([
-    'w-full md:w-[70%] xl:w-[60%] collapse collapse-arrow bg-card_bg/75 backdrop-blur-[100px] rounded-box z-10'
+    'w-full md:w-[70%] xl:w-[60%] collapse collapse-arrow bg-card_bg/75 backdrop-blur-[100px] rounded-xl z-10'
 ]) }}>
     {{ $slot }}
 </div>
